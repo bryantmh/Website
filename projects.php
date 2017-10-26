92f1ab49264477bf402f04a46523c014db55c049
Bryant Hinton <bryanthinton01@gmail.com>
2017-10-26 22:31:25 +0000

    Add new file

---
+++ projects.php
@@ -0,0 +1,53 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="script.js" ></script>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<body>
+	<ul>
+		<li><a href="./index.php">Home</a></li>
+		<li><a href="./projects.php">Projects</a></li>
+		<li><a href="https://gitlab.com/bryanthinton01">Git</a></li>
+		<li><a href="./about">About</a></li>
+	</ul><br>
+	<div class='parent'>
+		<p> Hey! Welcome to my website! It doesn't do anything useful yet.... but the boxes move when you resize the window, so that's pretty cool.</p>
+	    <div class='box'>
+	        <!-- <p class='box2'>This is a test textbox</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>This is another test textbox.....</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>And another</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>And another.....</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>And.... another</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>Another???</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>Okay, last one now, I promise....</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>LOL, JK</p> -->
+	    </div>
+	    <div class='box'>
+	        <!-- <p class='box2'>This is the last one</p> -->
+	    </div>
+	   <!--  <div class='box'>
+	        <input type='button' value="Button" onclick="testFunction()" style="width: 100%; height: 100%;">
+	    </div> -->
+	    <div class='box'>
+	        <p class='box2' id='add'></p>
+	    </div>
+	    <!-- <div class='box'>
+	        <input type='button' value="Button" onclick="testFunction()" style="width: 100%; height: 100%;">
+	    </div> -->
+	</div>
+</body>
+
+<a href='http://www.freepik.com/free-vector/grey-linen-texture-background_835399.htm' style="font-size: .6em; float: right; color: black;">Background by Freepik</a>
