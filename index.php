77b007828288132fd8971c3a06d6fa00b5276fcd
Bryant Hinton <bryanthinton01@gmail.com>
2017-10-26 21:00:07 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
 <body>
 	<ul>
-		<li><a href="">Stuff</a></li>
-		<li><a href="">Things</a></li>
-		<li><a href="">box</a></li>
-		<li><a href="">About</a></li>
+		<li><a href="./index.php">Home</a></li>
+		<li><a href="./projects.php">Projects</a></li>
+		<li><a href="./git.php">Git</a></li>
+		<li><a href="./about">About</a></li>
 	</ul><br>
 	<div class='parent'>
 		<p> Hey! Welcome to my website! It doesn't do anything useful yet.... but the boxes move when you resize the window, so that's pretty cool.</p>
