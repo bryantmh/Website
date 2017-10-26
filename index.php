42700404c50de845a6d3445bf5f6ec1d2dae2d4c
Bryant Hinton <bryanthinton01@gmail.com>
2017-10-26 22:30:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<ul>
 		<li><a href="./index.php">Home</a></li>
 		<li><a href="./projects.php">Projects</a></li>
-		<li><a href="./git.php">Git</a></li>
+		<li><a href="https://gitlab.com/bryanthinton01">Git</a></li>
 		<li><a href="./about">About</a></li>
 	</ul><br>
 	<div class='parent'>
