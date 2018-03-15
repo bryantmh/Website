9d20aa49e598108a1c4b842d4ca88ab58e82e522
Bryant Hinton <bryanthinton01@gmail.com>
2018-03-15 16:05:25 -0600

    Began work on Creative Project 4

---
+++ countdown/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('dist'));
+
+
+let items = [];
+let id = 0;
+
+app.get('/api/items/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  res.send(items[id]);
+});
+
+app.put('/api/items/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let itemsMap = items.map(item => { return item.id; });
+  let index = itemsMap.indexOf(id);
+  let item = items[index];
+  item.since = req.body.since;
+  item.from = req.body.from;
+  item.dateSelector = req.body.dateSelector;
+  res.send(item);
+});
+
+app.post('/api/items', (req, res) => {
+  id = id + 1;
+  let item = {id:id, text:req.body.text, completed: req.body.completed};
+  items.push(item);
+  res.send(id);
+});
+
+app.delete('/api/items/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let removeIndex = items.map(item => { return item.id; }).indexOf(id);
+  if (removeIndex === -1) {
+    res.status(404).send("Sorry, that item doesn't exist");
+    return;
+  }
+  items.splice(removeIndex, 1);
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
