d07ebe60942c787e068aa78a8ef05d1b7eb9ef5c
Bryant Hinton <bryantmh@fb-271-imaca.byu.edu>
2018-04-30 16:53:40 -0600

    Updated Home Page

--- main.php
+++ main.php
@@ -1,32 +1,59 @@
 <h2> Hey! Welcome to my website!</h2>
-<div class='box'>
-	<input type='button' value="Button" onclick="testFunction()" style="width: 100%; height: 100%;">
+<p>
+	BryantHinton.com is an assortment of pages that I have made, both for fun, as well as for classes I've taken at BYU<br>
+	I've utilized a large assortment of different technologies to build this website, a few of which are listed in the cards below
+</p>
+<div class="row">
+	<div class="col-sm-3">
+		<div class="card">
+			<img class="card-img-top cover" src="/img/img1.jpg" alt="Cool tree bro">
+			<div class="card-body">
+				<h4 class="card-title">HTML and CSS</h4>
+				<p class="card-text">These are the backbone of modern web development</p>
+				<a href="./other" class="btn btn-primary">Example</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<div class="card">
+			<img class="card-img-top cover" src="/img/img2.jpg" alt="Spaaaaaace">
+			<div class="card-body">
+				<h4 class="card-title">Bootstrap</h4>
+				<p class="card-text">This is a CSS framework that makes designing websites easier</p>
+				<a href="./portfolio" class="btn btn-primary">Example</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<div class="card">
+			<img class="card-img-top cover" src="/img/img3.jpg" alt="Here it is, the sunset!">
+			<div class="card-body">
+				<h4 class="card-title">PHP</h4>
+				<p class="card-text">For processing code before it is sent to your device. Files end in .php</p>
+				<a href="./photograpy/display.php" class="btn btn-primary">Example</a>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<div class="card">
+			<img class="card-img-top cover" src="/img/img4.jpg" alt="The Mun">
+			<div class="card-body">
+				<h4 class="card-title">JavaScript</h4>
+				<p class="card-text">For processing code once it's reached your device. Use jQuery for simplicity</p>
+				<a href="./astronomy" class="btn btn-primary">Example</a>
+			</div>
+		</div>
+	</div>
 </div>
-<div class='box'>
-	<!-- <p class='box2'>This is another test textbox.....</p> -->
+<div class="row">
+	<div class="col-sm-3">
+		<div class="card">
+			<img class="card-img-top cover" src="/img/img5.jpg" alt="Cold day">
+			<div class="card-body">
+				<h4 class="card-title">VueJS</h4>
+				<p class="card-text">A JavaScript framework that helps with larger applications</p>
+				<a href="./countdown" class="btn btn-primary">Example</a>
+			</div>
+		</div>
+	</div>
 </div>
-<div class='box'>
-	<!-- <p class='box2'>And another</p> -->
-</div>
-<div class='box'>
-	<!-- <p class='box2'>And another.....</p> -->
-</div>
-<div class='box'>
-	<!-- <p class='box2'>And.... another</p> -->
-</div>
-<div class='box'>
-	<!-- <p class='box2'>Another???</p> -->
-</div>
-<div class='box'>
-	<!-- <p class='box2'>Okay, last one now, I promise....</p> -->
-</div>
-<div class='box'>
-	<!-- <p class='box2'>LOL, JK</p> -->
-</div>
-<div class='box'>
-	<!-- <p class='box2'>This is the last one</p> -->
-</div>
-<div class='box'>
-</div>
-<br><br>
-<div id="add"></div>
